7aeb54afff09bff84608788ae04a6bc34a9ae0fe
Irfanwani <irfanwani347@gmail.com>
2023-02-18 22:24:17 +0530

    added action sheet for image selection

--- src/screens/detailscreens/generaldetails.tsx
+++ src/screens/detailscreens/generaldetails.tsx
@@ -1,21 +1,65 @@
-import { Avatar, Icon, IconButton, View } from "native-base";
+import {
+  Avatar,
+  Icon,
+  IconButton,
+  ScrollView,
+  View,
+  useDisclose,
+  Actionsheet,
+  HStack,
+  Box,
+  Text,
+} from "native-base";
 import { FC } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import styles from "./styles";
 
 const GeneralDetails: FC = () => {
+  const { isOpen, onClose, onOpen } = useDisclose();
   return (
-    <View alignSelf="center" paddingY="30">
-      <Avatar size="2xl">
-        <Icon as={Ionicons} name="person" size="2xl" />
-      </Avatar>
-      <IconButton
-        variant="solid"
-        icon={<Icon as={Ionicons} name="camera" size="2xl" />}
-        style={styles.iconbutton}
-        colorScheme="teal"
-      />
-    </View>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      keyboardShouldPersistTaps="always"
+      contentContainerStyle={styles.scroll}
+    >
+      <View alignSelf="center" paddingY="30">
+        <Avatar size="2xl">
+          <Icon as={Ionicons} name="person" size="2xl" />
+        </Avatar>
+        <IconButton
+          variant="solid"
+          icon={<Icon as={Ionicons} name="camera" size="2xl" />}
+          style={styles.iconbutton}
+          colorScheme="teal"
+          onPress={onOpen}
+        />
+      </View>
+
+      <Actionsheet isOpen={isOpen} onClose={onClose}>
+        <Actionsheet.Content>
+          <HStack space={20} p={3}>
+            <IconButton
+              variant="solid"
+              icon={<Icon as={Ionicons} name="camera" size="2xl" />}
+              colorScheme="teal"
+              rounded="full"
+            />
+            <IconButton
+              variant="solid"
+              icon={<Icon as={Ionicons} name="image-sharp" size="2xl" />}
+              colorScheme="rose"
+              rounded="full"
+            />
+            <IconButton
+              variant="solid"
+              icon={<Icon as={Ionicons} name="reload" size="2xl" />}
+              colorScheme="red"
+              rounded="full"
+            />
+          </HStack>
+        </Actionsheet.Content>
+      </Actionsheet>
+    </ScrollView>
   );
 };
 export default GeneralDetails;
--- src/screens/detailscreens/styles.ts
+++ src/screens/detailscreens/styles.ts
@@ -2,6 +2,9 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   iconbutton: { borderRadius: 40, position: "absolute", bottom: 25, right: 0 },
+  scroll: {
+    padding: 10
+  }
 });
 
 export default styles;
