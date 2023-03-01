ab83b50f1ce6f9f5fe3e6dd47faa8f259af282e3
Irfanwani <irfanwani347@gmail.com>
2023-03-01 14:29:50 +0530

    added custom logout hook

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,34 +1,16 @@
 import { FC, memo } from "react";
 import AuthMain from "./authscreens/main";
 import DetailsMain from "./detailscreens/main";
-import { deleteItemAsync } from "expo-secure-store";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Button } from "native-base";
-import authApiSlice, {
-  useLogoutMutation,
-} from "../store/apislices/authapislices";
-import { errorHandler } from "../utils/errorhandler";
-import { showToast } from "../components/generalcomponents/alerts";
+import { useLogout } from "../utils/customhooks";
 
 const Main: FC = () => {
   const { verified } = useSelector((state: any) => ({
     verified: state.authApiSlice?.mutations?.logindata?.data?.verified,
   }));
 
-  const [lg, { isLoading }] = useLogoutMutation();
-
-  const dispatch = useDispatch();
-
-  const logout = async () => {
-    try {
-      await lg(null).unwrap();
-      await deleteItemAsync("token");
-      dispatch(authApiSlice.util.resetApiState());
-      showToast("info", "Logged out successfully");
-    } catch (err) {
-      errorHandler(err);
-    }
-  };
+  const [logout, isLoading] = useLogout();
 
   return (
     <>
--- src/utils/customhooks.ts
+++ src/utils/customhooks.ts
@@ -1,5 +1,12 @@
+import { deleteItemAsync } from "expo-secure-store";
 import { useState, useEffect } from "react";
 import { Keyboard } from "react-native";
+import { useDispatch } from "react-redux";
+import { showToast } from "../components/generalcomponents/alerts";
+import authApiSlice, {
+  useLogoutMutation,
+} from "../store/apislices/authapislices";
+import { errorHandler } from "./errorhandler";
 
 export const useKeyboardVisible = () => {
   const [isKeyboardVisible, setKeyboardVisible] = useState(false);
@@ -25,4 +32,26 @@ export const useKeyboardVisible = () => {
   }, []);
 
   return isKeyboardVisible;
+};
+
+interface LogoutProps {
+  (): [() => Promise<void>, boolean];
+}
+
+export const useLogout: LogoutProps = () => {
+  const [lg, { isLoading }] = useLogoutMutation();
+
+  const dispatch = useDispatch();
+  const logout = async () => {
+    try {
+      await lg(null).unwrap();
+      await deleteItemAsync("token");
+      dispatch(authApiSlice.util.resetApiState());
+      showToast("info", "Logged out successfully");
+    } catch (err) {
+      errorHandler(err);
+    }
+  };
+
+  return [logout, isLoading];
 };
