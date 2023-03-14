2edb0c980c44cb85ce03b881a84655f5b28dfe65
Irfanwani <irfanwani347@gmail.com>
2023-03-14 20:02:45 +0530

    added transition to theming

--- src/components/generalcomponents/transition.tsx
+++ src/components/generalcomponents/transition.tsx
@@ -1,19 +1,44 @@
 import {
   Center,
   Heading,
+  Icon,
   Modal,
   PresenceTransition,
   useColorMode,
   useTheme,
 } from "native-base";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Dimensions } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
 import { TransitionProps } from "./types";
+import { Ionicons } from "@expo/vector-icons";
 
 const { width, height } = Dimensions.get("window");
 export const CustomTransition: FC<TransitionProps> = ({ loading }) => {
   const { colorMode } = useColorMode();
   const theme = useTheme();
+
+  const rotate = useSharedValue("0deg");
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotateZ: rotate.value }],
+    marginBottom: 10,
+  }));
+
+  useEffect(() => {
+    rotate.value = withRepeat(
+      withTiming("360deg", { duration: 750 }),
+      -1,
+      true
+    );
+    if (!loading) rotate.value = "0deg";
+  }, [loading]);
 
   const bgcolor = (cm: string) =>
     colorMode == cm ? theme.colors.light[600] : theme.colors.dark[600];
@@ -21,10 +46,8 @@ export const CustomTransition: FC<TransitionProps> = ({ loading }) => {
     <Modal isOpen={loading}>
       <PresenceTransition
         style={{
-          position: "absolute",
-          zIndex: 99999,
           borderRadius: height,
-          width: height,
+          width,
           height,
         }}
         visible={loading}
@@ -36,7 +59,7 @@ export const CustomTransition: FC<TransitionProps> = ({ loading }) => {
           opacity: 1,
           scale: 3,
           transition: {
-            duration: 250,
+            duration: 300,
           },
         }}
         exit={{
@@ -48,6 +71,14 @@ export const CustomTransition: FC<TransitionProps> = ({ loading }) => {
         }}
       >
         <Center w="full" h="full" bg={bgcolor("light")} borderRadius="full">
+          <Animated.View style={animatedStyle}>
+            <Icon
+              color={bgcolor("dark")}
+              as={Ionicons}
+              name="settings-outline"
+              size="sm"
+            />
+          </Animated.View>
           <Heading color={bgcolor("dark")} size="sm" fontSize="5">
             {`Switching to ${colorMode == "dark" ? "light" : "dark"} mode`}
           </Heading>
