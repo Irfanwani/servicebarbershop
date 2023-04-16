580ec2a634e44e7cddeaf366779b7e196a5b69d9
Irfanwani <irfanwani347@gmail.com>
2023-04-16 13:20:04 +0530

    fixed err handler

--- src/utils/errorhandler.tsx
+++ src/utils/errorhandler.tsx
@@ -1,8 +1,10 @@
+import { deleteItemAsync } from "expo-secure-store";
 import { showToast } from "../components/generalcomponents/alerts";
 import authApiSlice from "../store/apislices/authapislices";
+import { logoutaction } from "../store/slice";
 import { store } from "../store/store";
 
-export const errorHandler = (err: any) => {
+export const errorHandler = async (err: any) => {
   console.log(JSON.stringify(err), "error");
 
   try {
@@ -12,6 +14,8 @@ export const errorHandler = (err: any) => {
     if (!Number(status)) throw new Error();
     if (Number(status) == 401) {
       showToast("info", "Token Expired! Please login again");
+      store.dispatch(logoutaction());
+      await deleteItemAsync("token");
       store.dispatch(authApiSlice.util.resetApiState());
     } else {
       showToast("error", errorString);
